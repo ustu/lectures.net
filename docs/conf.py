e0925546f760344d02fa266750b1b104414c9283
Svintsov Dmitry <root@uralbash.ru>
2017-06-09 12:42:19 +0500

    fix conf

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../_lectures/docs/'))
 
-from config_sphinx import *  # noqa
+from config_sphinx import *  # noqa isort:skip
 
 project = u'Сетевое программирование'
 html_title = project
